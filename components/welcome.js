22703e4d984af26e6efafdcdd0932c385a118a2c
Ryan <ryanpsuresh@gmail.com>
2017-03-25 21:16:47 -0500

    add render method to welome.js component

--- components/welcome.js
+++ components/welcome.js
@@ -10,4 +10,36 @@ export class Welcome extends React.Component {
       style: { position: 'absolute' },
     }
   };
+  render() {
+    const { navigate } = this.props.navigation; 
+    return (
+       <View style={{flex: 1}}>
+        <Image
+          source={images.world5}
+          resizeMode='cover'
+          style={styles.container}
+        >
+          <View style={styles.titleContianer}>
+            <Text style={styles.title}>
+              Explorer
+            </Text>
+          </View>
+          <View style={styles.lastWorkoutContainer}>
+            <Text style={styles.lastWorkoutTitle}>
+              Discover the wonders of the world in the palm of your hand
+            </Text>
+          </View>
+          <View style={{padding: 30 }}>
+            <Button
+              onPress={() => navigate('Places')}
+              title="Places"
+              style={styles.button}
+              textStyle={styles.buttonText}
+              children={'Start Button'}
+            />
+          </View>
+        </Image>
+      </View>
+    )
+  }
 }
