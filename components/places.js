5a856219c55d00bd488edceaea8fe3c80b1d14fd
Ryan <ryanpsuresh@gmail.com>
2017-03-25 21:24:52 -0500

    add onButtonPress functionality

--- components/places.js
+++ components/places.js
@@ -11,3 +11,10 @@ const locationData = {
   '5': [require('../assets/cancun.jpg'), 'https://www.google.com/maps/uv?hl=en&pb=!1s0x8f5138c6e391c0e7:0x7c1ea0a168994d9a!2m19!2m2!1i80!2i80!3m1!2i20!16m13!1b1!2m2!1m1!1e1!2m2!1m1!1e3!2m2!1m1!1e5!2m2!1m1!1e4!3m1!7e115!4s/maps/place/chichen%2Bitza%2Bgoogle%2Bmaps/@20.6828739,-88.5680857,3a,75y,259.4h,90t/data%3D*213m4*211e1*213m2*211spvkFEJz1z6--L2-bVXunHw*212e0*214m2*213m1*211s0x0:0x7c1ea0a168994d9a!5schichen+itza+google+maps+-+Google+Search&imagekey=!1e2!2spvkFEJz1z6--L2-bVXunHw&sa=X&ved=0ahUKEwiK7t_5uO3SAhUD52MKHStMBOcQpx8IdzAO'],
   '6': [require('../assets/rome.jpg'), 'https://www.instantstreetview.com/@41.889962,12.492078,24.56h,5p,1z']
 }
+
+const onButtonPress = (id) => {
+  this.setState({
+    location: locationData[id][1],
+  });
+  navigate('map', {location: this.state.location});
+}
