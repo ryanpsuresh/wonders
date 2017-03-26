2600720de5e1c058cd98b31ac2fd12fb67cf585c
Ryan <ryanpsuresh@gmail.com>
2017-03-25 21:18:59 -0500

    add assets folder and images

--- components/welcome.js
+++ components/welcome.js
@@ -4,6 +4,16 @@ import Button from 'apsl-react-native-button';
 import { StackNavigator } from 'react-navigation';
 import { Text, View, StyleSheet, Image } from 'react-native';
 
+const images = {
+  'world': require('../assets/giphy.gif'),
+  'world2': require('../assets/giphy2.gif'),
+  'world3': require('../assets/giphy3.gif'),
+  'world4': require('../assets/giphy4.gif'),
+  'world5': require('../assets/giphy5.gif'),
+  'world6': require('../assets/giphy6.gif'),
+  'world7': require('../assets/giphy7.gif'),
+}
+
 export class Welcome extends React.Component {
   static navigationOptions = {
     header: {
