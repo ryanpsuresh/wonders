f10b5ee32ccf0f9b0413dd7c0e519b0af1f7bfc1
Ryan <ryanpsuresh@gmail.com>
2017-03-25 21:10:52 -0500

    add react navigation dependency

--- components/welcome.js
+++ components/welcome.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import Button from 'apsl-react-native-button';
+import { StackNavigator } from 'react-navigation';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
 
