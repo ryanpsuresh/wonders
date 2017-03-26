4776633ce2f28a88c80ba5365909972891ff7853
Ryan <ryanpsuresh@gmail.com>
2017-03-25 21:21:35 -0500

    alter index.ios.js setup

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native';
+import { Welcome } from './components/welcome.js';
+import { Places } from './components/places.js';
+import { StackNavigator } from 'react-navigation';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const ExplorerApp = StackNavigator({
+  Home: { screen: Welcome },
+  Places: { screen: Places }
+})
 
-export default class wonders extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('wonders', () => wonders);
+AppRegistry.registerComponent('wonders', () => ExplorerApp);
